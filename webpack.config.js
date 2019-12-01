7a708a10a880c37b9f391528472805deeb972df5
mcc <andi.m.mcclure@gmail.com>
2019-11-30 23:33:37 -0500

    Add package-lock.json, remove lingering signs of React

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,6 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
     },
 
     plugins: [
