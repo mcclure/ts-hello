aadb7b0788ef86ed4a6b2ea24d1734ded4b900af
mcc <andi.m.mcclure@gmail.com>
2020-09-19 13:07:20 -0400

    Fork for immutable/state tools

---
+++ src/state.tsx
@@ -0,0 +1,55 @@
+import { h, render, Context, createContext, JSX, ComponentChildren } from "preact";
+import { useContext, useState, useEffect } from "preact/hooks";
+
+// A piece of state with a notification system for Preact listeners
+class State<T> {
+  public context: Context<T>
+  private listeners:Set<(_:T)=>void>
+
+  // Construct with an initial value. "Inherit" to use an existing context key
+  constructor(public value:T, inherit?: Context<T>) {
+    this.listeners = new Set<(_:T)=>void>()
+    this.context = inherit || createContext(value)
+  }
+
+  // Call to update value
+  set(value:T) {
+    this.value = value
+    for (let fn of this.listeners) {
+      fn(this.value)
+    }
+  }
+  // Convenience function (calls useContext, so must be called in React callstack)
+  // For reading outside React callstacks, read .value
+  get() {
+  	return useContext(this.context)
+  }
+
+  addListener(listener:(_:T)=>void) { this.listeners.add(listener) }
+  removeListener(listener:(_:T)=>void) { this.listeners.delete(listener) }
+
+  clone() { return new State(this.value, this.context) }
+}
+
+// Preact component which is an auto-updating Provider for a State
+function StateContext<T> (props:{state:State<T>, children:ComponentChildren}) {
+  let state = props.state
+  let [value, setValue] = useState(state.value)
+  let effect = useEffect(() => {
+    state.addListener(setValue)
+    return () => {
+      state.removeListener(setValue)
+    }
+  }, []) // Empty dependencies list so listener only registers once
+  return <state.context.Provider value={value}>{props.children}</state.context.Provider>
+}
+
+// Function to wrap a React component in several layers of StateContexts at once
+function WrapStateContexts(content:JSX.Element, states:State<any>[]) {
+  for (let s of states) {
+      content = <StateContext state={s}>{content}</StateContext>
+    }
+    return content
+}
+
+export { State, StateContext, WrapStateContexts }
