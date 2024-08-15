d7088827c91cdec3a5bfc439e7964465aff3c966
mcc <andi.m.mcclure@gmail.com>
2024-08-14 21:50:23 -0400

    Dynamically resize (but only once)

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,70 @@ import { h, render, Component } from "preact";
 
 declare let require:any
 
+let root:HTMLElement = null;
+const BOXES_ACROSS = 11;
+const boxes:HTMLDivElement[] = [];
+let firstTime:number = null;
+let boxSide:number = null;
+let baseX:number = null;
+let baseY:number = null;
+
+function fixBoxSize() {
+  const width = root.offsetWidth;
+  const height = root.offsetHeight;
+  console.log("Resize", width, height);
+
+  const square = Math.min(width, height);
+  boxSide = Math.floor(square/(BOXES_ACROSS*3-1+5));
+  baseX = Math.floor((width - square)/2);
+  baseY = Math.floor((height - square)/2);
+
+  const size = boxSide + "px";
+  for (const idx in boxes) {
+    const box = boxes[idx];
+    box.style.width = size;
+    box.style.height = size;
+  }
+}
+
+function fixBoxPos() {
+  for (const _idx in boxes) {
+    const box = boxes[_idx];
+    const idx = +_idx;
+    const y = Math.floor(idx / BOXES_ACROSS);
+    const x = idx % BOXES_ACROSS;
+
+    box.style.left = (baseX + boxSide*3*(1+x)) + "px";
+    box.style.top = (baseY + boxSide*3*(1+y)) + "px";
+  }
+}
+
+function step(_frameTime:number) {
+  if (!firstTime)
+    firstTime = _frameTime;
+  const frameTime = _frameTime - firstTime; 
+
+  window.requestAnimationFrame(step);
+  fixBoxPos();
+}
+
 function boot() {
   console.log("Boot")
-  let root = document.getElementById("content");
-  let page = document.createTextNode("Loaded");
+  root = document.getElementById("content");
+  let page = document.createElement("div");
+
+  for (let z = 0; z < BOXES_ACROSS*BOXES_ACROSS; z++) {
+    let box = document.createElement("div");
+    box.style.position = "fixed";
+    box.style.backgroundColor = "white";
+
+    boxes.push(box);
+    page.appendChild(box);
+  }
+  fixBoxSize();
+  fixBoxPos();
   root.replaceChild(page, root.firstChild);
+  window.requestAnimationFrame(step);
 }
 
 (window as any).boot = boot; // Export
